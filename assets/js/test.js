a88568ee69c96f9eb78940d22f3c2deaece9f390
The-Digital-Space-YT <adrimaitra@gmail.com>
2022-02-03 21:32:01 -0800

    lots

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,3 +1,22 @@
-function revealMessage() {
-    document.getElementById("hiddenMessage").style.display= 'block';
+    /*let person = prompt("Please enter your name", "Harry Potter");
+    if (person == null) {
+      //document.getElementById("demo").innerHTML =
+      //"Hello " + person + "! How are you today?";
+      alert("Nothing");
+    }
+    else {
+        alert("Your Name: " + person);
+    }*/
+
+
+function message () {
+    var contactText = document.getElementById('contactMessage').value;
 }
+function subject () {
+    var contactEmail = document.getElementById('contactSubject').value;
+  }
+
+
+    var toemail = "baybasiyouthinitiative@gmail.com";
+    var subject = "baybasiyouthinitiative@gmail.com";
+    var cc = " baybasi@baybasi.us";
